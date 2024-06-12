ca773cd339cfc2d89cc44196c396c1033143fb39
mmmme <dlthdyd3@gmail.com>
2024-06-12 17:19:09 +0900

    TKP[サービステスト25％完了]

--- marketapp-master-master/src/test/java/com/marketapp/marketapp/service/ProductServiceTest.java
+++ marketapp-master-master/src/test/java/com/marketapp/marketapp/service/ProductServiceTest.java
@@ -1,13 +1,23 @@
 package com.marketapp.marketapp.service;
 
+import com.marketapp.marketapp.model.product;
 import com.marketapp.marketapp.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.awaitility.Awaitility.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,8 +29,25 @@ class ProductServiceTest {
     private ProductRepository p_repository;
 
 
+
+
     @Test
+    @DisplayName("すべての商品情報を表示")
     void showAllProducts() {
+        //given
+        List<product> p = new ArrayList<>();
+        product pt = new product(101,"test1",5, LocalDate.of(2024, 5, 31),"250",1101,1001);
+        product pt2 = new product(102,"test2",6, LocalDate.of(2024, 5, 25),"260",1102,1002);
+        p.add(pt);
+        p.add(pt2);
+
+        BDDMockito.given(p_repository.findAll()).willReturn(p);
+
+        //when
+        List<product> result = p_service.ShowAllProducts();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
     }
 
     @Test
